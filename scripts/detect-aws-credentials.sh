1a29d4d45a5e2c3712b6b820ce71e9c501df5164
Barrola <rbarrola@P2020020.local>
2018-10-12 06:28:18 -0700

    Add new script

--- scripts/detect-aws-credentials.sh
+++ scripts/detect-aws-credentials.sh
@@ -11,38 +11,32 @@ fi
 
 # Redirect output to stderr.
 exec 1>&2
- 
+
 # Check changed files for an AWS keys
 FILES=$(git diff --cached --name-only $against)
 
 if [ -n "$FILES" ]; then
-    KEY_ID=$(grep -E --line-number '[^A-Z0-9][A-Z0-9]{20}[^A-Z0-9]' $FILES)
-    KEY=$(grep -E --line-number '[^A-Za-z0-9/+=][A-Za-z0-9/+=]{40}[^A-Za-z0-9/+=]' $FILES)
+    ACCESS_KEY=$(grep -rE --line-number 'access_key' $FILES)
+    SECRET_KEY=$(grep -rE --line-number 'secret_key' $FILES)
 
-    if [ -n "$KEY_ID" ] || [ -n "$KEY" ]; then
+    if [ -n "$ACCESS_KEY" ] || [ -n "$SECRET_KEY" ]; then
         exec < /dev/tty # Capture input
         echo "=========== Possible AWS Access Key IDs ==========="
-        echo "${KEY_ID}"
+        echo "${ACCESS_KEY}"
         echo ""
 
         echo "=========== Possible AWS Secret Access Keys ==========="
-        echo "${KEY}"
+        echo "${SECRET_KEY}"
         echo ""
 
         while true; do
-            read -p "[AWS Key Check] Possible AWS keys found. Commit files anyway? (y/N) " yn
-            if [ "$yn" = "" ]; then
-                yn='N'
-            fi
-            case $yn in
-                [Yy] ) exit 0;;
-                [Nn] ) exit 1;;
-                * ) echo "Please answer y or n for yes or no.";;
-            esac
+
+                exit 1;
+
         done
-        exec <&- # Release input
+
     fi
 fi
 
-# Normal exit
+
 exit 0
